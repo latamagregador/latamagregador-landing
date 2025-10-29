8f341cfa45c36c4832d458d43dcd5b5103c071a6
latamagregador <contact@latamagregador.com>
2025-10-29 20:31:20 -0300

    Update index.jsx
    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main style={{minHeight: '100vh', display: 'grid', placeItems: 'center', background:'#0b1020', color:'#eaf0ff'}}>
+    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',background:'#0b1020',color:'#eaf0ff'}}>
       <div style={{textAlign:'center'}}>
         <h1 style={{margin:0}}>Latam Agregador</h1>
         <p>Landing deploy OK en Vercel</p>
